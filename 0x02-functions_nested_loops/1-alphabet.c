6d243a09f774aec5a644f8cbcfa1ecf19f00d84f
Nathnael Mesfin <nathnaelmesfin77@gmail.com>
2023-06-20 19:20:37 -0700

    _putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,7 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * print_alphabet - Prints the alphabet in lowercase
- *
- * Return : Always 0
+ * print_alphabet - prints the english alphabet from a-z.
+ * Return: Nothing.
  */
 void print_alphabet(void)
 {
@@ -13,4 +11,5 @@ void print_alphabet(void)
 	{
 		_putchar(c);
 	}
-}
+	_putchar('\n');
+}`
