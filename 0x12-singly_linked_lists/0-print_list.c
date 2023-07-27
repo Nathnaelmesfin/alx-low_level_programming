75b5878d46bfd56de2ec95b23f75e2cb51b2dad2
Nathnael Mesfin <nathnaelmesfin77@gmail.com>
2023-07-27 11:39:07 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,26 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
-* print_list - prints all the elements of a list
-*
-* @h:pointer to the list
-*
-*Return: the number of nodes
-*/
-
+  * print_list - prints all elements of a list
+  * @h: a list to print elements of
+  * Return: the number of nodes in list
+  */
 size_t print_list(const list_t *h)
-	{
-	const list_t *cursor = h;
-	size_t count = 0;
+{
+	int i = 0, ret = 1;
+	char nul[] = "(nil)";
 
-	while (cursor != NULL)
-	{
-	if (cursor->str != NULL)
-		printf("[%d] %s\n", cursor->len, cursor->str);
+	if (!h)
+		return (0);
+	if (h->str)
+		i = _strlen(h->str);
+	printf("[%d] ", i);
+
+	if (!h->str)
+		printf("%s\n", nul);
 	else
-		printf("[0] (nil)\n");
-		count += 1;
-		cursor = cursor->next;
-	}
-	return (count);
+		printf("%s\n", h->str);
+	if (h->next)
+		ret += print_list(h->next);
+	return (ret);
+}
+
+/**
+ * _strlen - finds string length
+ * @str: string
+ * Return: length of string
+ */
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i);)
+		i++;
+	return (i);
 }
