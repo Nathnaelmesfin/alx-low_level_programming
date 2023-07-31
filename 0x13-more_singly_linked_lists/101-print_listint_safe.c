a72b38a9d128b5e21a03255ba208d802f21c9d09
Nathnael Mesfin <nathnaelmesfin77@gmail.com>
2023-08-01 00:48:58 +0300

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * free_listp - frees a linked list
- * @head: head of a list.
+ * free_listp - frees linked list
+ * @head: head of list.
  *
  * Return: no return.
  */
@@ -25,13 +25,13 @@ void free_listp(listp_t **head)
 
 /**
  * print_listint_safe - prints a linked list.
- * @head: head of a list.
+ * @head: head of list.
  *
- * Return: number of nodes in the list.
+ * Return: no of nodes in a list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nnodes = 0;
+	size_t hnodes = 0;
 	listp_t *hptr, *new, *add;
 
 	hptr = NULL;
@@ -55,15 +55,15 @@ size_t print_listint_safe(const listint_t *head)
 			{
 				printf("-> [%p] %d\n", (void *)head, head->n);
 				free_listp(&hptr);
-				return (nnodes);
+				return (hnodes);
 			}
 		}
 
 		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
-		nnodes++;
+		hnodes++;
 	}
 
 	free_listp(&hptr);
-	return (nnodes);
+	return (hnodes);
 }
