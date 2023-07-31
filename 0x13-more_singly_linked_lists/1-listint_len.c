adb7fbd1be04a5e74907a563f8ae6068f34bcd16
Nathnael Mesfin <nathnaelmesfin77@gmail.com>
2023-08-01 01:29:06 +0300

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,6 +7,11 @@
  *
  * Return: no of nodes.
  */
+typedef struct listint_s {
+    int data;
+    struct listint_s *next;
+} listint_t;
+
 size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
