754d2f56eb730e409080adbf3665ec433ee2c709
Nathnael Mesfin <nathnaelmesfin77@gmail.com>
2023-08-07 10:43:07 -0700

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,35 +3,36 @@
 /**
  * create_file - creates a file
  * @filename: filename.
- * @text_content: content written in the file.
+ * @text_content: content writed in the file.
  *
  * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int a;
-	int noletters;
-	int b;
+	int fd;
+	int nletters;
+	int rwr;
 
 	if (!filename)
 		return (-1);
 
-	a = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (a == -1)
+	if (fd == -1)
 		return (-1);
 
 	if (!text_content)
 		text_content = "";
 
-	for (noletters = 0; text_content[noletters]; noletters++);
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
 
-	b = write(a, text_content, noletters);
+	rwr = write(fd, text_content, nletters);
 
-	if (b == -1)
+	if (rwr == -1)
 		return (-1);
 
-	close(a);
+	close(fd);
 
 	return (1);
 }
